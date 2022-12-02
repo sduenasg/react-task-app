4e9f8556522bc0aea538461aab1a74377629e351
Sara Dueñas <sduenas@institutorobotica.org>
2022-12-02 12:52:38 +0100

    Tailwind css

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -19,9 +19,11 @@ function TaskForm() {
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="max-w-md mx-auto">
+      <form className="bg-slate-800 p-10 mb-4" onSubmit={handleSubmit}>
+        <h1 className="text-2xl font-bold text-white mb-3">Create your task</h1>
         <input
+          className="bg-slate-300 p-3 w-full"
           placeholder="Input your task"
           onChange={(e) => {
             setTitle(e.target.value);
@@ -30,13 +32,14 @@ function TaskForm() {
           autoFocus
         />
         <textarea
+          className="bg-slate-300 p-3 w-full"
           placeholder="Input your task's description"
           onChange={(e) => {
             setDescription(e.target.value);
           }}
           value={description}
         />
-        <button>Save</button>
+        <button className="bg-indigo-500 px-3 py-1 text-white hover:bg-indigo-300">Save</button>
       </form>
     </div>
   );
